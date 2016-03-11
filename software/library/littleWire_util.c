7cc09779c6aae011798dbaa161e1fbcdbd95e9d0
fuji pebri <pebri86@gmail.com>
2016-03-11 13:18:02 +0700

    add library

--- software/library/littleWire_util.c
+++ software/library/littleWire_util.c
@@ -1,12 +1,12 @@
 
-#include <littleWire_util.h>
+#include "littleWire_util.h"
 
 /* Delay in miliseconds */
 void delay(unsigned int duration)
 {
-	#ifdef LINUX
-		usleep(duration*1000);
-	#else
-		Sleep(duration);
-	#endif
+#ifdef LINUX
+    usleep(duration*1000);
+#else
+    Sleep(duration);
+#endif
 }
--- software/library/littleWire_util.h
+++ software/library/littleWire_util.h
@@ -1,11 +1,11 @@
 #ifndef LITTLEWIRE_UTIL_H
 #define LITTLEWIRE_UTIL_H
 
-#ifdef LINUX
-	#include <unistd.h>
-#else
-	#include <windows.h>
-#endif
+//#ifdef LINUX
+#include <unistd.h>
+//#else
+//	#include <windows.h>
+//#endif
 
 /* Delay in miliseconds */
 void delay(unsigned int duration);
